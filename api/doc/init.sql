169ba6d73d588d6506852513979cb6ef279fd8b8
tan <13320385140@163.com>
2025-11-17 10:52:30 +0800

    init

---
+++ api/doc/init.sql
@@ -0,0 +1,2 @@
+-- 用户
+INSERT INTO `db_block_flow`.`users` (`id`, `create_time`, `update_time`, `email`, `is_active`, `last_login_time`, `password`, `real_name`, `role`, `username`) VALUES (1, '2025-11-17 10:51:11.158470', '2025-11-17 10:51:11.158470', '550019013@qq.com', b'1', NULL, '14159', '谭宁', 'USER', 'admin');
