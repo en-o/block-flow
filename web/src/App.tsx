1c2c9f6c7730a1956b4f191d5f9cec36a1376ce1
tan <13320385140@163.com>
2025-11-18 13:02:24 +0800

    123

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { App as AntdApp } from 'antd';
 import Login from './pages/Login';
 import Flow from './pages/Flow';
 import Manage from './pages/Manage';
@@ -11,37 +12,39 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 登录页面 */}
-        <Route path="/login" element={<Login />} />
+    <AntdApp>
+      <BrowserRouter>
+        <Routes>
+          {/* 登录页面 */}
+          <Route path="/login" element={<Login />} />
 
-        {/* Flow页面 - 无需鉴权 */}
-        <Route path="/flow" element={<Flow />} />
+          {/* Flow页面 - 无需鉴权 */}
+          <Route path="/flow" element={<Flow />} />
 
-        {/* 管理后台 - 需要鉴权 */}
-        <Route
-          path="/manage"
-          element={
-            <PrivateRoute>
-              <Manage />
-            </PrivateRoute>
-          }
-        >
-          {/* 默认重定向到块管理 */}
-          <Route index element={<Navigate to="/manage/blocks" replace />} />
-          <Route path="blocks" element={<Blocks />} />
-          <Route path="block-types" element={<BlockTypes />} />
-          <Route path="context" element={<Context />} />
-        </Route>
+          {/* 管理后台 - 需要鉴权 */}
+          <Route
+            path="/manage"
+            element={
+              <PrivateRoute>
+                <Manage />
+              </PrivateRoute>
+            }
+          >
+            {/* 默认重定向到块管理 */}
+            <Route index element={<Navigate to="/manage/blocks" replace />} />
+            <Route path="blocks" element={<Blocks />} />
+            <Route path="block-types" element={<BlockTypes />} />
+            <Route path="context" element={<Context />} />
+          </Route>
 
-        {/* 默认重定向到Flow页面 */}
-        <Route path="/" element={<Navigate to="/flow" replace />} />
+          {/* 默认重定向到Flow页面 */}
+          <Route path="/" element={<Navigate to="/flow" replace />} />
 
-        {/* 404页面 */}
-        <Route path="*" element={<Navigate to="/flow" replace />} />
-      </Routes>
-    </BrowserRouter>
+          {/* 404页面 */}
+          <Route path="*" element={<Navigate to="/flow" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AntdApp>
   );
 };
 
